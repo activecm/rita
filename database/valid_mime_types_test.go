8940b0ca0fe7a7cf2282473ade53ad77b9ecf143
Paweł Floryan <81591050+pawelfloryan@users.noreply.github.com>
2026-02-16 16:28:00 +0100

    Added xml mime types data to test case

--- database/valid_mime_types_test.go
+++ database/valid_mime_types_test.go
@@ -63,7 +63,9 @@ func TestReadValidTextMIMETypeFile(t *testing.T) {
 			dataCSV: `plain,text/plain,".txt, .ps",[RFC2046][RFC3676][RFC5147]
 					  markdown,text/markdown,".md, .markdown",[RFC7763]
 					  javascript,text/javascript,".js, .mjs., es, .mjs",[RFC9239]
-					  html,text/html,none,[RFC21221]`,
+					  html,text/html,none,[RFC21221]
+					  xml,text/xml," .xml, .xsd, .ent, .dtd, .mod",[RFC7303]
+					  xml,application/xml," .xml, .xsd, .ent, .dtd, .mod",[RFC7303]`,
 			writeChan: make(chan Data),
 			expectedMIMETypes: []*ValidMIMEType{
 				{
@@ -102,8 +104,48 @@ func TestReadValidTextMIMETypeFile(t *testing.T) {
 					MIMEType:  "text/html",
 					Extension: "",
 				},
+				{
+					MIMEType:  "text/xml",
+					Extension: "xml",
+				},
+				{
+					MIMEType:  "text/xml",
+					Extension: "xsd",
+				},
+				{
+					MIMEType:  "text/xml",
+					Extension: "ent",
+				},
+				{
+					MIMEType:  "text/xml",
+					Extension: "dtd",
+				},
+				{
+					MIMEType:  "text/xml",
+					Extension: "mod",
+				},
+				{
+					MIMEType:  "application/xml",
+					Extension: "xml",
+				},
+				{
+					MIMEType:  "application/xml",
+					Extension: "xsd",
+				},
+				{
+					MIMEType:  "application/xml",
+					Extension: "ent",
+				},
+				{
+					MIMEType:  "application/xml",
+					Extension: "dtd",
+				},
+				{
+					MIMEType:  "application/xml",
+					Extension: "mod",
+				},
 			},
-			expectedTotal: 9,
+			expectedTotal: 19,
 			expectedError: false,
 		},
 		{
